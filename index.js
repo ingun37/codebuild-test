8486b9245c5740b6e0d04b3f9afb25b19c518f06
Ingun Jon <ingun@foxxing.com>
2020-11-30 15:56:32 +0900

    aoeu-

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const path = require('path')
+const fs = require("fs");
+
+const binPath = puppeteer.executablePath()
+
+const dirPath = path.dirname(binPath)
+const libPath = path.resolve(dirPath, "libEGL.so")
+
+puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-web-security"],
+}).then(browser => {
+    return browser.newPage().then(page=>{
+        return page.goto("https://threejs.org/examples/#webgl_geometry_cube")
+    }).then(response => {
+        return new Promise(done => {
+            setTimeout(done, 5000);
+        })
+    }).then(()=>{
+        return browser.close()
+    })
+})
