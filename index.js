f846aae19c52fe74a32f6eeecf1cd30769b1c931
Ingun Jon <ingun@foxxing.com>
2020-11-30 19:11:06 +0900

    gen pdf

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const puppeteer = require('puppeteer');
 const path = require('path')
 const fs = require("fs");
-
+const { execFileSync } = require('child_process');
 const binPath = puppeteer.executablePath()
 
-const dirPath = path.dirname(binPath)
-const libPath = path.resolve(dirPath, "libEGL.so")
+execFileSync(binPath, ["--headless", "--use-gl=egl", "--print-to-pdf", "chrome://gpu"])
 
-puppeteer.launch({
-    // headless: false,
-    args: ["--no-sandbox", "--disable-web-security"],
-}).then(browser => {
-    return browser.newPage().then(page=>{
-        return page.goto("https://threejs.org/examples/#webgl_geometry_cube")
-    }).then(response => {
-        return new Promise(done => {
-            setTimeout(done, 5000);
-        })
-    }).then(()=>{
-        return browser.close()
-    })
-})
+// const dirPath = path.dirname(binPath)
+// const libPath = path.resolve(dirPath, "libEGL.so")
+
+// puppeteer.launch({
+//     // headless: false,
+//     args: ["--no-sandbox", "--disable-web-security"],
+// }).then(browser => {
+//     return browser.newPage().then(page=>{
+//         return page.goto("https://threejs.org/examples/#webgl_geometry_cube")
+//     }).then(response => {
+//         return new Promise(done => {
+//             setTimeout(done, 5000);
+//         })
+//     }).then(()=>{
+//         return browser.close()
+//     })
+// })
